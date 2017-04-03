9f98e743e5e1bad11201d182ef94612df223694e
Keerthik <keerthiko@gmail.com>
2017-04-03 05:48:55 -0700

    added facebook pixel, fixed responsive design, updated press kit release date to Q3

--- _includes/press/Color Balls/facts.md
+++ _includes/press/Color Balls/facts.md
@@ -2,7 +2,7 @@
 [{{ site.developer }}]({{ site.developer_url }})
 
 ### Release Date
-Unreleased (expected Q2 2017)
+Unreleased (expected Q3 2017)
 
 ### Platforms
 Expected: Steam (PC/Mac)
--- js/tracking.js
+++ js/tracking.js
@@ -1,5 +1,21 @@
 // placeholder script file for analytics/tracking stuff
+$(document).ready(function() {
+	console.log("Document loaded");
+	$('#greenlight_button').click(function(e) {
+		greenlightPressed();
+	});
 
-function greenlightPressed() {
-	fbq('track', 'Greenlight Lead');	
-}
+	$('.greenlight_web').click(function(e) {
+		greenlightLinked(e.target);
+	});
+
+	function greenlightPressed() {
+		console.log("Pressing steam link");
+		fbq('trackCustom', 'Greenlight Steam Lead');
+	}
+
+	function greenlightLinked(target) {
+		console.log("Pressing web link: ", target);
+		fbq('trackCustom', 'Greenlight Web Lead', {'element': target});
+	}
+});
